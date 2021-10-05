4d841797a320ef56ea1d639b8473e5c949f8a800
tehnarenok <tehnarenok@gmail.com>
2021-10-05 07:47:59 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,105 @@
+# React global store (react-gstore)
+This library is intended for creating a global store in react
+## Install
+```shell
+npm i react-gstore
+```
+## Examples
+### 1 store
+```javascript
+import React, { useState } from "react"
+import { createContainer } from "react-gstore"
+import { render } from "react-dom"
+ 
+function useTheme(initialState = 0) {
+    let [theme, setTheme] = useState(initialState)
+    let dark = () => setTheme(1)
+    let light = () => setTheme(0)
+    let anotherTheme = () => setTheme((theme + 1) % 2)
+    return { theme, dark, light, anotherTheme}
+}
+ 
+let Theme = createContainer(useTheme)
+ 
+function MyBlock() {
+    let theme = Theme.useContainer()
+    return (
+        <div style={{backgroundColor: theme.theme === 0 ? 'white' : '#333333'}}>
+            <p>
+                Hello world
+            </p>
+            <button onClick={theme.anotherTheme}>
+                Change theme
+            </button>
+        </div>
+    )
+}
+ 
+function App() {
+    return (
+        <Theme.Provider initialState={1}>
+            <MyBlock />
+        </Theme.Provider>
+    )
+}
+ 
+render(<App />, document.getElementById("root"))
+```
+### 2 and more stores
+```javascript
+import React, { useState } from "react"
+import { createContainer, Provider } from "react-gstore"
+import { render } from "react-dom"
+ 
+function useTheme(initialState = 0) {
+    let [theme, setTheme] = useState(initialState)
+    let dark = () => setTheme(1)
+    let light = () => setTheme(0)
+    let anotherTheme = () => setTheme((theme + 1) % 2)
+    return { theme, dark, light, anotherTheme}
+}
+ 
+function useCounter(initialState = 0) {
+    let [count, setCount] = useState(initialState)
+    let decrement = () => setCount(count - 1)
+    let increment = () => setCount(count + 1)
+    return { count, decrement, increment }
+}
+ 
+let Theme = createContainer(useTheme)
+let Counter = createContainer(useCounter)
+ 
+function MyBlock() {
+    let counter = Counter.useContainer()
+    let theme = Theme.useContainer()
+    return (
+        <div style={{backgroundColor: theme.theme === 0 ? 'white' : '#333333', color: theme.theme === 1 ? 'white' : '#333333'}}>
+            <p>
+                Hello world
+            </p>
+            <button onClick={theme.anotherTheme}>
+                Change theme
+            </button>
+            <p>
+                Counter
+            </p>
+            <button onClick={counter.decrement}>-</button>
+                <span>{counter.count}</span>
+            <button onClick={counter.increment}>+</button>
+        </div>
+    )
+}
+ 
+function App() {
+    return (
+        <Provider containers={[
+            {container: Theme, initialState: 1},
+            {container: Counter, initialState: 0}
+        ]}>
+            <MyBlock />
+        </Provider>
+    )
+}
+ 
+render(<App />, document.getElementById("root"))
+```
---
+++ src/react-gstore.tsx
@@ -0,0 +1,108 @@
+import React from "react"
+
+const EMPTY: unique symbol = Symbol()
+
+export interface ContainerProviderProps<State = void> {
+	initialState?: State
+	children: React.ReactNode
+}
+
+export interface Container<Value, State = void> {
+	Provider: React.ComponentType<ContainerProviderProps<State>>
+	useContainer: () => Value
+}
+
+export interface ProviderProps {
+    containers: Array<{container: Container<any, any>, initialState? : any}>
+    children?: React.ReactNode
+}
+
+const saveToLocalStorage = (func: Function, prefix?: string, name?: string) => {
+    return (...args : any) => {
+        let localStorageName : string = `${prefix ? prefix : ''}` + `${name ? name : func.name}`
+
+        let str : string | null = null
+        if(localStorageName) str = localStorage.getItem(localStorageName)
+        let argsFromStorage = str ? JSON.parse(str) : null
+
+        let result = func.apply(undefined, argsFromStorage ? [argsFromStorage] : args)
+
+        if(localStorageName) {
+            localStorage.setItem(localStorageName, JSON.stringify(result))
+        }
+        return result
+    }
+}
+
+const saveToSessionStorage = (func: Function, prefix?: string, name?: string) => {
+    return (...args : any) => {
+        let sessionStorageName : string = `${prefix ? prefix : ''}` + `${name ? name : func.name}` 
+
+        let str : string | null = null
+        if(sessionStorageName) str = sessionStorage.getItem(sessionStorageName)
+        let argsFromStorage = str ? JSON.parse(str) : null
+
+        let result = func.apply(undefined, argsFromStorage ? [argsFromStorage] : args)
+
+        if(sessionStorageName) {
+            localStorage.setItem(sessionStorageName, JSON.stringify(result))
+        }
+        return result
+    }
+}
+
+export function createContainer<Value, State = void>(
+    useHook: (initialState?: State) => Value, 
+    saveToStorage? : boolean, 
+    saveConfig? : {type?: string, prefix?: string, name?: string}
+    ): Container<Value, State> {
+    
+    if(saveToStorage) {
+        let saveType = saveConfig?.type? saveConfig.type : 'local'
+        let prefix = saveConfig?.prefix? saveConfig.prefix : undefined
+        let name = saveConfig?.name? saveConfig.name : undefined
+        if(saveType === 'local') useHook = saveToLocalStorage(useHook, prefix, name)
+        if(saveType === 'session') useHook = saveToSessionStorage(useHook, prefix, name)
+    }
+
+	let Context = React.createContext<Value | typeof EMPTY>(EMPTY)
+
+	function Provider(props: ContainerProviderProps<State>) {
+		let value = useHook(props.initialState)
+		return <Context.Provider value={value}>{props.children}</Context.Provider>
+	}
+
+	function useContainer(): Value {
+		let value = React.useContext(Context)
+		if (value === EMPTY) {
+			throw new Error("Component must be wrapped with <Container.Provider>")
+		}
+		return value
+	}
+
+	return { Provider, useContainer }
+}
+
+export function Provider (props : ProviderProps) {
+    const rec = (props: ProviderProps) : any => {
+        if(props.containers.length === 0) {
+            return props.children
+        }
+        let item : {container: Container<any, any>, initialState? : any} | undefined = props.containers.shift()
+        if(!item) {
+            return rec(props)
+        }
+        return (
+            <item.container.Provider initialState={item.initialState}>
+                {rec(props)}
+            </item.container.Provider>
+        )
+    }
+    return rec(props)
+}
+
+export function useContainer<Value, State = void>(
+	container: Container<Value, State>,
+): Value {
+	return container.useContainer()
+}
